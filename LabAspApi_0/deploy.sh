f572cc0c28e4510bc45f06babf9c0c6bf484fe7d
2PECTRE <iam2pectre@gmail.com>
2024-11-23 03:01:33 +0900

    Update deploy.sh

--- LabAspApi_0/deploy.sh
+++ LabAspApi_0/deploy.sh
@@ -30,7 +30,7 @@ fi
 set -e
 
 ### 로그 기록
-LOG_DIR="/etc/log"
+LOG_DIR="$(dirname "$0")/etc/log"
 mkdir -p "$LOG_DIR"
 
 LOG_FILE="$LOG_DIR/deploy_$(date +'%Y%m%d_%H%M%S').log"
@@ -72,7 +72,7 @@ if ! docker info >/dev/null 2>&1; then
     exit 1
 fi
 
-HASH_DIR="/etc/hash"
+HASH_DIR="$(dirname "$0")/etc/hash"
 mkdir -p "$HASH_DIR"
 
 ### Windows 환경에 맞춘 경로
